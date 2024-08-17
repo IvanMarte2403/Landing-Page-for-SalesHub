28ea0ce549fed22b963f44291391f7e769747c2d
IvanMarte <ivanbiasfx@gmail.com>
2024-08-16 22:05:43 -0600

    update: team-seaction

--- main/selector-clients.js
+++ main/selector-clients.js
@@ -8,3 +8,14 @@ document.querySelectorAll('.container-selector').forEach(item => {
         item.classList.add('selected');
     });
 });
+
+document.querySelectorAll('.container-team-selector').forEach(item => {
+    item.addEventListener('click', () => {
+        document.querySelectorAll('.container-team-selector').forEach(el => {
+            el.classList.remove('selected');
+            el.classList.add('unselected');
+        });
+        item.classList.remove('unselected');
+        item.classList.add('selected');
+    });
+});
